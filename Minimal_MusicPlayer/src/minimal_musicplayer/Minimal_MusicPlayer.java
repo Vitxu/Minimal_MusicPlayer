cb8b4d99d2257179e996cb3b09a27ce53727c1b7
human <human@machine.home>
2017-07-20 20:17:44 +0200

    Initializing Project

--- Minimal_MusicPlayer/src/minimal_musicplayer/Minimal_MusicPlayer.java
+++ Minimal_MusicPlayer/src/minimal_musicplayer/Minimal_MusicPlayer.java
@@ -10,8 +10,10 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 
 /**
  *
@@ -22,18 +24,20 @@ public class Minimal_MusicPlayer extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        TextField textField = new TextField ();
+        btn.setText("Play some funk");
         btn.setOnAction(new EventHandler<ActionEvent>() {
-            
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                PruebaMetodos.playSomething(textField.getText());
+                System.out.println("Playing sound from the path " + textField.getText());
             }
         });
         
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
+        textField.setTranslateY(-20);
+        btn.setTranslateY(20);
+        root.getChildren().addAll(btn, textField);
         Scene scene = new Scene(root, 300, 250);
         
         primaryStage.setTitle("Hello World!");
